fadda4d1c8a6cda5d92a7a3a88c0ac2fd7af22b0
durgachepuri <60810870+durgachepuri@users.noreply.github.com>
2020-02-18 10:35:51 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,31 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exercise
+namespace Cirle
 {
-    public class counting
+    class Program
     {
         static void Main(string[] args)
         {
-            string inp = Console.ReadLine();
-            string[] Words = inp.Split(' ');
-            Dictionary<string, int> data = new Dictionary<string, int>();
-            for (int n = 0; n < Words.Length; ++n)
+            Circle cir = new Circle();
+            cir.r = 10;
+            cir.c.x = 6;
+            cir.c.y = 3;
+            Console.WriteLine(cir.area() + "" + cir.c.y);      
+        }
+        public class Circle
+        {
+            public int r;
+            public point c = new point();
+            public double area()
             {
-                String temp = Words[n];
-                if (data.ContainsKey(temp))
-                {
-                    data[temp] = data[temp] + 1;
-                }
-                else
-                {
-                    data.Add(temp, 1);
-                }
-            }
-            foreach (string n in data.Keys)
-            {
-                Console.WriteLine("Word:{0} - Freq:{1}", n, data[n]);
+                return r * r * 3.14;
             }
         }
-    }
+        public class point
+        {
+            public int x;
+            public int y;
+        }
+    }   
 }

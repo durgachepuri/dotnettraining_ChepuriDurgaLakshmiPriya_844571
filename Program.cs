934a15344b590b968165cf5c5a01b6538b42ae5c
durgachepuri <60810870+durgachepuri@users.noreply.github.com>
2020-02-18 10:39:23 +0530

    Word Frequency
    using System;
    using System.Collections.Generic;
    using System.Linq;
    using System.Text;
    using System.Threading.Tasks;
    
    namespace exercise
    {
        public class counting
        {
            static void Main(string[] args)
            {
                string inp = Console.ReadLine();
                string[] Words = inp.Split(' ');
                Dictionary<string, int> data = new Dictionary<string, int>();
                for (int n = 0; n < Words.Length; ++n)
                {
                    String temp = Words[n];
                    if (data.ContainsKey(temp))
                    {
                        data[temp] = data[temp] + 1;
                    }
                    else
                    {
                        data.Add(temp, 1);
                    }
                }
                foreach (string n in data.Keys)
                {
                    Console.WriteLine("Word:{0} - Freq:{1}", n, data[n]);
                }
            }
        }
    }

--- Program.cs
+++ Program.cs
@@ -4,31 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cirle
+namespace exercise
 {
-    class Program
+    public class counting
     {
         static void Main(string[] args)
         {
-            Circle cir = new Circle();
-            cir.r = 10;
-            cir.c.x = 6;
-            cir.c.y = 3;
-            Console.WriteLine(cir.area() + "" + cir.c.y);      
-        }
-        public class Circle
-        {
-            public int r;
-            public point c = new point();
-            public double area()
+            string inp = Console.ReadLine();
+            string[] Words = inp.Split(' ');
+            Dictionary<string, int> data = new Dictionary<string, int>();
+            for (int n = 0; n < Words.Length; ++n)
             {
-                return r * r * 3.14;
+                String temp = Words[n];
+                if (data.ContainsKey(temp))
+                {
+                    data[temp] = data[temp] + 1;
+                }
+                else
+                {
+                    data.Add(temp, 1);
+                }
+            }
+            foreach (string n in data.Keys)
+            {
+                Console.WriteLine("Word:{0} - Freq:{1}", n, data[n]);
             }
         }
-        public class point
-        {
-            public int x;
-            public int y;
-        }
-    }   
+    }
 }
